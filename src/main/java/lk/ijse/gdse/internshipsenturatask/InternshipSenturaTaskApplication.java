950ad9e5af73d239e68e5249800bcb774c74676f
tharindu-dasun <tharindu.dasunccp@gmail.com>
2024-02-03 09:26:06 +0530

    Initial commit

---
+++ src/main/java/lk/ijse/gdse/internshipsenturatask/InternshipSenturaTaskApplication.java
@@ -0,0 +1,13 @@
+package lk.ijse.gdse.internshipsenturatask;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InternshipSenturaTaskApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InternshipSenturaTaskApplication.class, args);
+    }
+
+}
---
+++ src/test/java/lk/ijse/gdse/internshipsenturatask/InternshipSenturaTaskApplicationTests.java
@@ -0,0 +1,13 @@
+package lk.ijse.gdse.internshipsenturatask;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InternshipSenturaTaskApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
